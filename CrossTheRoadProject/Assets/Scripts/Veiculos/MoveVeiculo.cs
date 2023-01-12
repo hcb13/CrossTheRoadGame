4dc3a57bdda8c56d169c6999c0ecf134ac1e00a7
Hortênsia Costa de Barcelos <hcb13@users.noreply.github.com>
2023-01-12 17:22:50 -0300

    version 2
    More vehicles
    
    Vehicles are moving for one point to another, and respawn in the initial position

--- CrossTheRoadProject/Assets/Scripts/Veiculos/MoveVeiculo.cs
+++ CrossTheRoadProject/Assets/Scripts/Veiculos/MoveVeiculo.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MoveVeiculo : MonoBehaviour
 {
     [SerializeField]
     private WaypointsVeiculos waypoints;
+    [SerializeField]
+    private float speed = 2f;
 
     public WaypointsVeiculos Waypoints
     {
@@ -13,6 +16,65 @@ public class MoveVeiculo : MonoBehaviour
         set { waypoints = value; }
     }
 
+    private Rigidbody2D _rigidbody;
+    private bool isGoingRight = false;
 
 
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypoints.GetWaypointInitial.position.x > 0)
+        {
+            // going left
+            isGoingRight = false;
+        }
+        else
+        {
+            isGoingRight = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(transform.position, waypoints.GetWaypointEnd.position) < 0.9f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isGoingRight)
+        {
+            MoveRight();
+        }
+        else
+        {
+            MoveLeft();
+        }
+    }
+
+    private void MoveLeft()
+    {
+
+        _rigidbody.velocity = Vector2.left * speed;
+    }
+
+    private void MoveRight()
+    {
+
+        _rigidbody.velocity = Vector2.right * speed;
+
+    }
 }
+
--- CrossTheRoadProject/Assets/Scripts/VeiculosPool/CreateVeiculosPool.cs
+++ CrossTheRoadProject/Assets/Scripts/VeiculosPool/CreateVeiculosPool.cs
@@ -12,33 +12,72 @@ public class CreateVeiculosPool : MonoBehaviour
     private List<WaypointsVeiculos> waypoints;
 
     [SerializeField]
-    private int amount = 1;
+    private int amountPerLane = 2;
+
+    [SerializeField]
+    private float gapInstantiate = 3f;
 
     private void Start()
     {
         InitializePool();
+        InvokeRepeating("GetVeiculoFromPool", 1f, gapInstantiate);
     }
 
     private void InitializePool()
     {
         int target = 0;
-        int waypointTarget = 0;
-        for(int i = 0; i < amount; i++)
+        for(int i = 0; i < amountPerLane; i++)
         {
-            target = Random.Range(0, prefabs.Count);
-            waypointTarget = Random.Range(0, waypoints.Count);
-            AddVeiculoToPool(target, waypointTarget);
+            for (int waypointTarget = 0; waypointTarget < waypoints.Count; waypointTarget++)
+            {
+                target = Random.Range(0, prefabs.Count);
+
+                AddVeiculoToPool(target, waypointTarget);
+            }
         }
     }
 
     private void AddVeiculoToPool(int target, int waypointTarget)
     {
-        GameObject gameObject = Instantiate(prefabs[target], waypoints[waypointTarget].GetWaypointInitial.position, Quaternion.identity, transform);
+        GameObject gameObject = Instantiate(prefabs[target], waypoints[waypointTarget].GetWaypointInitial.position, Quaternion.identity, waypoints[waypointTarget].transform);
         if(gameObject.transform.position.x > 0)
         {
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
         gameObject.GetComponent<MoveVeiculo>().Waypoints = waypoints[waypointTarget];
+        gameObject.SetActive(false);
+    }
+
+    private GameObject GetVeiculoFromPool()
+    {
+        MoveVeiculo veiculo = null;
+
+        for (int i = 0; i < waypoints.Count; i++)
+        {
+            for(int j = 0; j < waypoints[i].transform.childCount; j++)
+            {
+                veiculo = waypoints[i].transform.GetChild(j).GetComponent<MoveVeiculo>();
+                if(veiculo != null)
+                {
+                    if (!veiculo.gameObject.activeSelf)
+                    {
+                        break;
+                    }
+                }
+            }
+            if(veiculo != null)
+            {
+                if (!veiculo.gameObject.activeSelf)
+                {
+                    veiculo.transform.position = veiculo.Waypoints.GetWaypointInitial.position;
+                    veiculo.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        
+
+        return veiculo.gameObject;
     }
 
 }
